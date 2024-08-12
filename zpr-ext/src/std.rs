5f5855084fe90834218243bff549b0155c2d414a
Chris Pacejo <cpacejo@appliedinvention.com>
2024-08-12 17:38:01 +0200

    zpr-ext: implement very basic ancillary data support (#310)

--- zpr-ext/src/std.rs
+++ zpr-ext/src/std.rs
@@ -140,6 +140,266 @@ pub mod mem {
     }
 }
 
+pub mod os {
+    pub mod unix {
+        pub mod net {
+            use libc;
+            use std::io::{Error, IoSlice, IoSliceMut, Result};
+            use std::os::fd::{AsRawFd, RawFd};
+            use std::os::unix::net::UnixStream;
+            use std::ptr;
+            use std::vec::Vec;
+
+            #[cfg(any(doc, target_os = "android", target_os = "linux"))]
+            pub struct SocketAncillary<'a> {
+                buffer: &'a mut [u8],
+                fds: Vec<RawFd>,
+            }
+
+            impl<'a> SocketAncillary<'a> {
+                pub fn new(buffer: &'a mut [u8]) -> Self {
+                    Self {
+                        buffer,
+                        fds: Vec::new(),
+                    }
+                }
+
+                pub fn add_fds(&mut self, fds: &[RawFd]) -> bool {
+                    self.fds.extend_from_slice(fds);
+                    true // FIXME: it's a lie!
+                }
+
+                pub fn clear(&mut self) {
+                    self.fds.clear();
+                }
+
+                pub fn is_empty(&self) -> bool {
+                    self.fds.is_empty()
+                }
+
+                pub fn messages(&self) -> Messages<'_> {
+                    if self.fds.is_empty() {
+                        None.into_iter()
+                    } else {
+                        Some(Ok(AncillaryData::ScmRights(self.fds.iter().cloned()))).into_iter()
+                    }
+                }
+            }
+
+            pub type Messages<'a> =
+                std::option::IntoIter<std::result::Result<AncillaryData<'a>, AncillaryError>>;
+
+            pub enum AncillaryData<'a> {
+                ScmRights(ScmRights<'a>),
+                ScmCredentials(ScmCredentials<'a>),
+            }
+
+            pub type AncillaryError = ();
+
+            pub type ScmRights<'a> = std::iter::Cloned<std::slice::Iter<'a, RawFd>>;
+
+            pub struct ScmCredentials<'a>(std::marker::PhantomData<&'a ()>);
+
+            pub trait UnixStreamExt {
+                #[cfg(any(doc, target_os = "android", target_os = "linux"))]
+                fn send_vectored_with_ancillary(
+                    &self,
+                    bufs: &[IoSlice<'_>],
+                    ancillary: &mut SocketAncillary<'_>,
+                ) -> Result<usize>;
+
+                #[cfg(any(doc, target_os = "android", target_os = "linux"))]
+                fn recv_vectored_with_ancillary(
+                    &self,
+                    bufs: &mut [IoSliceMut<'_>],
+                    ancillary: &mut SocketAncillary<'_>,
+                ) -> Result<usize>;
+            }
+
+            impl UnixStreamExt for UnixStream {
+                /// This is a very silly and limited implementation of `send_vectored_with_ancillary()` as we await
+                /// stabilization of [unix_socket_ancillary_data](https://github.com/rust-lang/rust/issues/76915).
+                #[cfg(any(doc, target_os = "android", target_os = "linux"))]
+                fn send_vectored_with_ancillary(
+                    &self,
+                    bufs: &[IoSlice<'_>],
+                    ancillary: &mut SocketAncillary<'_>,
+                ) -> Result<usize> {
+                    let fds_size = std::mem::size_of_val(&ancillary.fds[..]);
+
+                    let mut msghdr = libc::msghdr {
+                        msg_name: ptr::null_mut(),
+                        msg_namelen: 0,
+                        msg_iov: bufs.as_ptr() as *mut _,
+                        msg_iovlen: bufs.len(),
+                        msg_control: ptr::null_mut(),
+                        msg_controllen: 0,
+                        msg_flags: 0,
+                    };
+
+                    if fds_size > 0 {
+                        unsafe {
+                            msghdr.msg_control = (&mut ancillary.buffer).as_mut_ptr().cast();
+                            msghdr.msg_controllen = libc::CMSG_SPACE(fds_size as _) as _;
+
+                            let cmsghdr = libc::CMSG_FIRSTHDR(&msghdr);
+                            (*cmsghdr).cmsg_len = libc::CMSG_LEN(fds_size as _) as _;
+                            (*cmsghdr).cmsg_level = libc::SOL_SOCKET;
+                            (*cmsghdr).cmsg_type = libc::SCM_RIGHTS;
+                            libc::CMSG_DATA(cmsghdr).copy_from_nonoverlapping(
+                                (&ancillary.fds[..]).as_ptr().cast(),
+                                fds_size,
+                            );
+                        }
+                    }
+
+                    let res = unsafe { libc::sendmsg(self.as_raw_fd(), &msghdr, 0) };
+
+                    if res > 0 {
+                        Ok(res as usize)
+                    } else {
+                        Err(Error::last_os_error())
+                    }
+                }
+
+                #[cfg(any(doc, target_os = "android", target_os = "linux"))]
+                fn recv_vectored_with_ancillary(
+                    &self,
+                    bufs: &mut [IoSliceMut<'_>],
+                    ancillary: &mut SocketAncillary<'_>,
+                ) -> Result<usize> {
+                    let mut msghdr = libc::msghdr {
+                        msg_name: ptr::null_mut(),
+                        msg_namelen: 0,
+                        msg_iov: bufs.as_ptr() as *mut _,
+                        msg_iovlen: bufs.len(),
+                        msg_control: (&mut ancillary.buffer).as_mut_ptr().cast(),
+                        msg_controllen: ancillary.buffer.len() as _,
+                        msg_flags: 0,
+                    };
+
+                    let res = unsafe { libc::recvmsg(self.as_raw_fd(), &mut msghdr, 0) };
+
+                    unsafe {
+                        let cmsghdr = libc::CMSG_FIRSTHDR(&msghdr);
+                        if !cmsghdr.is_null()
+                            && (*cmsghdr).cmsg_level == libc::SOL_SOCKET
+                            && (*cmsghdr).cmsg_type == libc::SCM_RIGHTS
+                        {
+                            let size_of_hdr = (2
+                                * libc::CMSG_LEN(std::mem::size_of::<RawFd>() as _)
+                                - libc::CMSG_LEN((2 * std::mem::size_of::<RawFd>()) as _))
+                                as usize;
+                            let num_fds =
+                                ((*cmsghdr).cmsg_len - size_of_hdr) / std::mem::size_of::<RawFd>();
+                            ancillary.fds.clear();
+                            ancillary.fds.extend_from_slice(std::slice::from_raw_parts(
+                                libc::CMSG_DATA(cmsghdr).cast(),
+                                num_fds,
+                            ));
+                        }
+                    }
+
+                    if res > 0 {
+                        Ok(res as usize)
+                    } else {
+                        Err(Error::last_os_error())
+                    }
+                }
+            }
+
+            #[cfg(test)]
+            #[allow(unstable_name_collisions)]
+            mod tests {
+                use super::*;
+                use std::fs::File;
+                use std::io::Read;
+                use std::os::fd::{AsRawFd, FromRawFd};
+                use std::os::unix::net::UnixStream;
+
+                #[test]
+                fn ancillary_no_ancillary() {
+                    let (s1, s2) = UnixStream::pair().unwrap();
+
+                    let data_in = &[1u8, 2u8, 3u8];
+                    let mut ancillary_in_buf = [0u8; 256];
+                    let mut ancillary_in = SocketAncillary::new(&mut ancillary_in_buf);
+                    assert_eq!(
+                        s1.send_vectored_with_ancillary(
+                            &[IoSlice::new(data_in)],
+                            &mut ancillary_in
+                        )
+                        .unwrap(),
+                        3
+                    );
+
+                    let mut data_out = [0u8; 4];
+                    let mut ancillary_out_buf = [0u8; 256];
+                    let mut ancillary_out = SocketAncillary::new(&mut ancillary_out_buf);
+                    assert_eq!(
+                        s2.recv_vectored_with_ancillary(
+                            &mut [IoSliceMut::new(&mut data_out[..])],
+                            &mut ancillary_out
+                        )
+                        .unwrap(),
+                        3
+                    );
+
+                    assert!(ancillary_out.messages().next().is_none());
+                }
+
+                #[test]
+                fn ancillary_fd() {
+                    let zero_file = File::open("/dev/zero").unwrap();
+
+                    let (s1, s2) = UnixStream::pair().unwrap();
+
+                    let data_in = &[1u8, 2u8, 3u8];
+                    let mut ancillary_in_buf = [0u8; 256];
+                    let mut ancillary_in = SocketAncillary::new(&mut ancillary_in_buf);
+                    ancillary_in.add_fds(&[zero_file.as_raw_fd()]);
+                    assert_eq!(
+                        s1.send_vectored_with_ancillary(
+                            &[IoSlice::new(data_in)],
+                            &mut ancillary_in
+                        )
+                        .unwrap(),
+                        3
+                    );
+
+                    let mut data_out = [0u8; 4];
+                    let mut ancillary_out_buf = [0u8; 256];
+                    let mut ancillary_out = SocketAncillary::new(&mut ancillary_out_buf);
+                    assert_eq!(
+                        s2.recv_vectored_with_ancillary(
+                            &mut [IoSliceMut::new(&mut data_out[..])],
+                            &mut ancillary_out
+                        )
+                        .unwrap(),
+                        3
+                    );
+
+                    let mut messages = ancillary_out.messages();
+
+                    let fds: Vec<_> = match messages.next().unwrap().unwrap() {
+                        AncillaryData::ScmRights(fds) => fds.collect(),
+                        AncillaryData::ScmCredentials(_) => panic!("expected ScmRights"),
+                    };
+                    assert!(fds.len() == 1);
+
+                    let mut buf = 123u8;
+                    unsafe { File::from_raw_fd(fds[0]) }
+                        .read_exact(std::slice::from_mut(&mut buf))
+                        .unwrap();
+                    assert_eq!(buf, 0u8);
+
+                    assert!(messages.next().is_none());
+                }
+            }
+        }
+    }
+}
+
 pub mod vec {
     pub trait VecExt<T> {
         fn recycle<U>(self) -> Vec<U>;
