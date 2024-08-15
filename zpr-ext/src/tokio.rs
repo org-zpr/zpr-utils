f5ddc669f996bf54a5fd612c64fd7731f4b1c1e1
Chris Pacejo <cpacejo@appliedinvention.com>
2024-08-15 19:31:05 +0200

    zpr-ext: attach_reuse_port_cbpf (#328)

--- zpr-ext/src/tokio.rs
+++ zpr-ext/src/tokio.rs
@@ -4,12 +4,14 @@ pub mod net {
     };
     use nix::sys::socket;
     use std::io::{self, IoSlice, IoSliceMut};
-    use std::os::fd::AsFd;
+    use std::os::fd::{AsFd, AsRawFd};
     use tokio::io::Interest;
     use tokio::net::{UdpSocket, UnixStream};
 
     pub trait UdpSocketExt {
         fn mtu(&self) -> io::Result<u32>;
+        #[cfg(any(doc, target_os = "android", target_os = "linux"))]
+        fn attach_reuse_port_cbpf(&self, filter: &[libc::sock_filter]) -> io::Result<()>;
     }
 
     impl UdpSocketExt for UdpSocket {
@@ -18,6 +20,34 @@ pub mod net {
             match socket::getsockopt(self, socket::sockopt::IpMtu) {
                 Ok(mtu) => Ok(mtu as u32),
                 Err(errno) => Err(io::Error::from(errno)),
+            }
+        }
+
+        #[cfg(any(doc, target_os = "android", target_os = "linux"))]
+        fn attach_reuse_port_cbpf(&self, filter: &[libc::sock_filter]) -> io::Result<()> {
+            let fprog = libc::sock_fprog {
+                len: filter.len() as u16,
+                filter: filter.as_ptr().cast_mut(),
+            };
+
+            let fprog_ptr = (&fprog as *const libc::sock_fprog).cast();
+            let fprog_size = std::mem::size_of_val(&fprog) as libc::socklen_t;
+
+            // SAFETY: `fprog_ptr` and `fprog_size` are of the appropriate type for SOL_SOCKET:SO_ATTACH_REUSEPORT_CBPF
+            let res = unsafe {
+                libc::setsockopt(
+                    self.as_raw_fd(),
+                    libc::SOL_SOCKET,
+                    libc::SO_ATTACH_REUSEPORT_CBPF,
+                    fprog_ptr,
+                    fprog_size,
+                )
+            };
+
+            if res < 0 {
+                Err(io::Error::last_os_error())
+            } else {
+                Ok(())
             }
         }
     }
