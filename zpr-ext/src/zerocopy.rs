4939a236fef7004aa5f6a079d47e26281635b0a9
Chris Pacejo <cpacejo@appliedinvention.com>
2024-10-08 12:54:20 -0400

    zpr-ext: port to zerocopy 0.8

--- zpr-ext/src/zerocopy.rs
+++ zpr-ext/src/zerocopy.rs
@@ -1,3 +1,27 @@
+pub mod error {
+    use std::marker::PhantomData;
+
+    /// NOTE: zerocopy currently provides no way to construct a
+    /// SizeError, so we must return an opaque error type.
+    #[non_exhaustive]
+    pub struct SizeError<Src, Dst: ?Sized>(PhantomData<zerocopy::error::SizeError<Src, Dst>>);
+
+    impl<Src, Dst: ?Sized> SizeError<Src, Dst> {
+        #[allow(dead_code)]
+        pub(super) fn new() -> Self {
+            Self(PhantomData)
+        }
+    }
+
+    impl<Src, Dst: ?Sized> std::fmt::Debug for SizeError<Src, Dst> {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            write!(f, "SizeError")
+        }
+    }
+}
+
+pub use error::*;
+
 #[cfg(feature = "bytes")]
 mod zerocopy_bytes {
     use crate::bytes::BufExt;
@@ -5,9 +29,14 @@ mod zerocopy_bytes {
     use std::mem::MaybeUninit;
     use zerocopy::*;
 
-    pub trait AsBytesExt {
+    pub trait IntoBytesExt {
         /// Like `write_to()`, but writes to a `BufMut`.
-        fn write_to_buf(&self, buf: &mut impl BufMut) -> Option<()>;
+        fn write_to_buf<B: BufMut>(
+            &self,
+            buf: &mut B,
+        ) -> Result<(), super::SizeError<&Self, &mut B>>
+        where
+            Self: Immutable;
     }
 
     pub trait FromBytesExt {
@@ -15,19 +44,25 @@ mod zerocopy_bytes {
         fn as_uninit_bytes_mut(&mut self) -> &mut [MaybeUninit<u8>];
 
         /// Like `read_from_prefix()`, but reads from a `Buf`.
-        fn read_from_buf(buf: &mut impl Buf) -> Option<Self>
+        fn read_from_buf<B: Buf>(buf: &mut B) -> Result<Self, super::SizeError<&mut B, Self>>
         where
             Self: Sized;
     }
 
-    impl<T: AsBytes> AsBytesExt for T {
-        fn write_to_buf(&self, buf: &mut impl BufMut) -> Option<()> {
+    impl<T: IntoBytes> IntoBytesExt for T {
+        fn write_to_buf<B: BufMut>(
+            &self,
+            buf: &mut B,
+        ) -> Result<(), super::SizeError<&Self, &mut B>>
+        where
+            Self: Immutable,
+        {
             let bytes = self.as_bytes();
             if buf.remaining_mut() < bytes.len() {
-                None
+                Err(super::SizeError::new())
             } else {
                 buf.put(bytes);
-                Some(())
+                Ok(())
             }
         }
     }
@@ -45,16 +80,16 @@ mod zerocopy_bytes {
             }
         }
 
-        fn read_from_buf(buf: &mut impl Buf) -> Option<Self>
+        fn read_from_buf<B: Buf>(buf: &mut B) -> Result<Self, super::SizeError<&mut B, Self>>
         where
             Self: Sized,
         {
             if buf.remaining() < std::mem::size_of::<Self>() {
-                None
+                Err(super::SizeError::new())
             } else {
                 let mut ret = T::new_zeroed();
                 buf.copy_to_maybe_uninit_slice_mut(ret.as_uninit_bytes_mut());
-                Some(ret)
+                Ok(ret)
             }
         }
     }
